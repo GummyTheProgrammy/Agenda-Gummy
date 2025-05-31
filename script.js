a90bac9ab74ff526db48bed80187fc04cc2da330
GummyTheProgrammy <32723033+GummyTheProgrammy@users.noreply.github.com>
2025-05-31 11:12:59 -0300

    atualização
    31/05 atualização de agenda

--- script.js
+++ script.js
@@ -63,22 +63,28 @@ class Compromisso {
 
 // Lista de compromissos
 const compromissos = [
-	//new Compromisso(VD, 'Ensaio', 'Violence Division', '01/05', '21h às 23h', 'Cafofo'),
-	//new Compromisso(Jardins, 'Ensaio', 'Jardins do Jack', '00/05', '20h', 'Vale dos cristais'),
-	new Compromisso(Groot, 'Show', 'Ostracycle', '25/04', '21h', 'Rio das Ostras'),
-	new Compromisso(ResCos, 'Show', 'Ostracycle', '26/04', '21h', 'Rio das Ostras'),
-	new Compromisso(Groot, 'Show', 'Ostracycle', '27/04', '14h', 'Rio das Ostras'),
-	new Compromisso(Cerv, 'Ensaio', 'Cervical', '28/04', '20h', 'Cafofo'),
-	new Compromisso(Groot, 'Show', 'Marrentos', '01/05', 'A definir', 'Onde?'),
-	new Compromisso(Cerv, 'Show', 'Heavy Beer', '04/05', 'A definir', 'Rio de Janeiro - Rua Ceará'),
-	new Compromisso(Jardins, 'Show', 'Tropic Thunder', '09/05', 'A definir', 'Rio das Ostras'),
-	new Compromisso(VD, 'Show', 'Macaé Motoclube', '17/05', 'A definir', 'Macaé'),
-	new Compromisso(Groot, 'Show', 'Macaé Motoclube', '17/05', 'A definir', 'Macaé'),
-	new Compromisso(Groot, 'Show', 'Rato de Ouro', '31/05', 'A definir', 'Rio das Ostras'),
-	new Compromisso(ResCos, 'Show', 'Rato de Ouro', '20/06', '20h', 'Rio das Ostras'),
+	//new Compromisso(VD, 'Ensaio', 'Violence Division', '17/03', '21h às 23h', 'Cafofo'),
+	//new Compromisso(Jardins, 'Ensaio', 'Jardins do Jack', '20/03', '20h', 'Cafofo'),
+	//new Compromisso(Cerv, 'Ensaio', 'Cervical', '27/03', '20h', 'Cafofo'),
+	//new Compromisso(Groot, 'Ensaio', 'Grootescos', '27/03', '20h', 'Cafofo'),
+	//new Compromisso(ResCos, 'Show', 'Rato de Ouro', '20/06', '20h', 'Rio das Ostras'),
+	//JUNHO
+	new Compromisso(Jardins, 'Show', 'Tropic Thunder', '07/06', '20h', 'Rio das Ostras'),
+	new Compromisso(Groot, 'Show', 'Rota das Ostras', '14/06', '15h', 'Rio das Ostras'),
+	new Compromisso(ResCos, 'Show', 'Feirinha do Mirante', '14/06', '19h', 'Rio das Ostras'),
+	//JULHO
+	new Compromisso(Groot, 'Show', 'Irmandade 1927', '05/07', 'Sem horario', 'Sem local'),
+	new Compromisso(VD, 'Show', 'Cabofrio Motorrock', '10/07 à 13/07', 'Sem horario', 'Sem local'),
+	new Compromisso(Cerv, 'Show', 'Hardcore', '11/07', 'Sem horário', 'Cachoeira Itapemirin'),	
+	new Compromisso(Cerv, 'Show', 'Rio 100 Bandas', '12/07', 'Sem horário', 'Rio de Janeiro'),	
+	new Compromisso(Groot, 'Show', 'Rato de Ouro Round 2', '31/07', 'A definir', 'Rio das Ostras'),
 	new Compromisso(Groot, 'Show', 'Arcada Moto Clube', '09/08', 'A definir', '???'),
-	new Compromisso(Groot, 'Show', 'Macaé Moto Week', '10/10', 'A definir', 'Macaé'),
+	new Compromisso(Groot, 'Show', 'Festival de Covers', '06/09', 'A definir', 'Rio das Ostras'),
+	new Compromisso(Groot, 'Show', 'Perpétuos MC', '12/09', 'A definir', 'Sem local'),
+	new Compromisso(Cerv, 'Show', 'Hardcore', '20/09', 'Sem horário', 'Ipatinga'),	
+	new Compromisso(Cerv, 'Show', 'Hardcore', '21/09', 'Sem horário', 'Campos'),	
 	new Compromisso(Groot, 'Show', 'Macaé Moto Week', '11/10', 'A definir', 'Macaé'),
+	new Compromisso(VD, 'Show', 'Warlords', '14/11', 'A definir', 'Rio de Janeiro'),
 	new Compromisso(Groot, 'Show', 'Família Catés', '06/12', 'A definir', '???'),
 ];
 
